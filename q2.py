74f264a08fdf872a16be54375362397acfa5de43
Ashutosh Singh <124326426+AshutoshSingh1s@users.noreply.github.com>
2026-01-19 17:24:53 +0530

    Remove unnecessary whitespace in compressed_stack_length

--- q2.py
+++ q2.py
@@ -67,13 +67,11 @@ def compressed_stack_length(lst):
     # TODO: Implement your solution here
   def compressed_stack_length(lst):
     stack = []
-    
     for num in lst:
         if stack and stack[-1] == num:
             stack.pop()
         else:
             stack.append(num)
-    
     return len(stack)
 if __name__ == "__main__":
     print(compressed_stack_length([1, 2, 2, 3]))    
